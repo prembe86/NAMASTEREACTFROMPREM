57534bd2dfc15ac4b50350e6afab5f1df25041ec
prembe86 <prembe86@gmail.com>
2023-11-23 00:13:14 -0600

    Notes

--- app.js
+++ app.js
@@ -1,45 +1,73 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
 
-const heading = React.createElement("h1", { id: "heading", className: "header" },
+// const heading = React.createElement("h1", { id: "heading", className: "header" },
 
 
 
-    "This is my first reach heading ");
+//     "This is my first reach heading ");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// const parent = React.createElement("div", { id: "parent" }, [
+
+//     React.createElement("div", { id: "child1" }, [
+//         React.createElement("h1", { id: "heading1", className: "header" },
+
+
+
+//             "This is my first reach heading "),
+//         React.createElement("h1", { id: "heading2", className: "header" },
+
+
+
+//             "This is my first reach heading ")
+
+
+//     ]),
+//         React.createElement("div", { id: "child2" }, [
+//             React.createElement("h1", { id: "heading1", className: "header" },
+
+
+
+//                 "This is my first reach heading "),
+//             React.createElement("h1", { id: "heading2", className: "header" },
+
+
+
+//                 "This is my first reach heading "),
+
+
+
+//         ])
+
+
+// ]);
+// root.render(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const Head = () => {return <h1 id="heading" className="header" tabIndex="4">JSX heading</h1>};
+const Head4 = () => (<h1 id="heading" className="header" tabIndex="4">JSX heading4</h1>);
+const Head3 = () => <h1 id="heading2" className="header" tabIndex="4">JSX heading3</h1>
+    const Head2 = () => {
 
-const parent = React.createElement("div", { id: "parent" }, [
-
-    React.createElement("div", { id: "child1" }, [
-        React.createElement("h1", { id: "heading1", className: "header" },
-
-
-
-            "This is my first reach heading "),
-        React.createElement("h1", { id: "heading2", className: "header" },
-
-
-
-            "This is my first reach heading ")
+       return (<h1 id="heading2" className="header" tabIndex="4">
+    
+    JSX heading2</h1>)
+    
+};
 
 
-    ]),
-        React.createElement("div", { id: "child2" }, [
-            React.createElement("h1", { id: "heading1", className: "header" },
-
-
-
-                "This is my first reach heading "),
-            React.createElement("h1", { id: "heading2", className: "header" },
-
-
-
-                "This is my first reach heading "),
-
-
-
-        ])
-
-
-]);
-root.render(parent);
+const heading = (<h1
+>heading<Head4/></h1>
+)
+    const Container = () =>
+        (<div id="container">
+            <Head/>
+            <Head2/>
+            <Head3/>
+            <Head4/>
+            <Head4> </Head4>
+            {Head4()}
+            {heading}
+        </div>)
+    
+root.render(<Container/>);
